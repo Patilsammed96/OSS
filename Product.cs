3a63d3068402cea0803ac2e15722c9c2a451a35e
Patilsammed96 <108923477+Patilsammed96@users.noreply.github.com>
2022-07-08 15:40:48 +0530

    I have added Product.cs file
    I have added Product.cs file

---
+++ Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFLWebApp.Models
+{
+
+    //POCO class
+    //Entity Class
+    //Data Annotations defined metadata for class or field, or property
+
+    [Table("Product10")]
+    public class Product
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ProductDesc { get; set; }
+       
+        public int ProductQuantity { get; set; }
+
+
+
+    }
+}
